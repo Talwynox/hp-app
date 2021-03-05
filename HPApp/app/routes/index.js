bb6648ea4570d4ac6fca990cc728a03376e7c62f
Georg Aschauer <georgeaschauer@gmail.com>
2021-03-05 12:14:20 +0100

    Components for MonsterList/Detail added

--- HPApp/app/routes/index.js
+++ HPApp/app/routes/index.js
@@ -1,6 +1,8 @@
 import Route from '@ember/routing/route';
+import DataTableRouteMixin from 'ember-data-table/mixins/route';
 
-export default Route.extend({
+export default Route.extend(DataTableRouteMixin, {
+    modelName: 'encouter',
     model(){
         return this.store.findAll('encounter');
     }

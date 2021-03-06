d608d38ac5fdb724917f16e59ca377b6222ae8a4
Georg Aschauer <georgeaschauer@gmail.com>
2021-03-06 08:45:31 +0100

    detail page added, routing fixed

--- HPApp/app/router.js
+++ HPApp/app/router.js
@@ -6,9 +6,10 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
 Router.map(function() {
+  this.route('about');
   this.route('monster', function(){
+    this.route('index', { path: '/'});
     this.route('details', { path: '/:monster_id' });
   });
 });
